e3a5048cd203e401597c400568e9cc29ddf2d1e1
Lucia Rodriguez <lucia.rodriguez@utec.edu.pe>
2026-09-24 18:33:15 -0500

    feat(common): add custom exception hierarchy based on ApiException

---
+++ src/main/java/com/examforge/api/common/exception/InvalidTokenException.java
@@ -0,0 +1,10 @@
+package com.examforge.api.common.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class InvalidTokenException extends ApiException {
+
+    public InvalidTokenException(String message) {
+        super(HttpStatus.UNAUTHORIZED, message);
+    }
+}
